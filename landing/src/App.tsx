835f6450f352223e3a1b6ac973ea63a89f0cde30
faizntfd <faizintfd@gmail.com>
2026-05-18 16:30:32 +0700

    feat: add custom landing scrollbar

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,3 +1,4 @@
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FeatureSection from './components/FeatureSection';
@@ -5,14 +6,25 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black text-white selection:bg-white/30 font-sans">
-      <Header />
-      <main>
-        <Hero />
-        <FeatureSection />
-      </main>
-      <Footer />
-    </div>
+    <OverlayScrollbarsComponent
+      className="h-screen bg-black text-white selection:bg-white/30 font-sans"
+      defer
+      options={{
+        scrollbars: {
+          autoHide: 'move',
+          theme: 'os-theme-termul',
+        },
+      }}
+    >
+      <div className="min-h-screen">
+        <Header />
+        <main>
+          <Hero />
+          <FeatureSection />
+        </main>
+        <Footer />
+      </div>
+    </OverlayScrollbarsComponent>
   );
 }
 
